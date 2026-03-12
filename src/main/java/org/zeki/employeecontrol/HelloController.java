7f866bc2435804d766e4fa3606e429d5568696c5
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2026-03-12 13:25:04 +0100

    Create base fx project

---
+++ src/main/java/org/zeki/employeecontrol/HelloController.java
@@ -0,0 +1,14 @@
+package org.zeki.employeecontrol;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
